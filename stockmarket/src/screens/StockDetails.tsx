2db6f80bd9ef989f4661beb17042ed4ca78d6b54
adrianacostep33 <adriana.coste@principal33.com>
2024-03-31 13:42:42 +0300

    Receive basic data in stock details screen

--- stockmarket/src/screens/StockDetails.tsx
+++ stockmarket/src/screens/StockDetails.tsx
@@ -1,10 +1,24 @@
-import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../Navigation/StockStack';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import Colors from '../constants/colors';
 
-const StockDetails = () => {
+type StockDetailsProps = NativeStackScreenProps<
+  RootStackParamList,
+  'StockDetails'
+>;
+
+const StockDetails = ({route}: StockDetailsProps) => {
+  const {stockId} = route.params;
+
+  useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>StockDetails</Text>
+      <Text style={styles.text}>Details: {stockId}</Text>
     </View>
   );
 };
@@ -16,6 +30,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: Colors.dark800,
   },
   text: {
     color: 'white',
